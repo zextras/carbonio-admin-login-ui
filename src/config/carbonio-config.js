7b9209b2596419cf0c565d826a8d3278c312b2a8
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-02-22 11:28:54 +0530

    fix: adjust the background image from config

--- src/config/carbonio-config.js
+++ src/config/carbonio-config.js
@@ -1,7 +1,7 @@
 export function carbonioConfig() {
 	return {
 		"publicUrl": "https://infra-6f4c8e62.testarea.zextras.com",
-		"loginPageBackgroundImage": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.freecreatives.com%2Fwp-content%2Fuploads%2F2016%2F02%2FAmazing-Road-Background-For-Free1.jpg",
+		"loginPageBackgroundImage": "", // "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.freecreatives.com%2Fwp-content%2Fuploads%2F2016%2F02%2FAmazing-Road-Background-For-Free1.jpg",
 		"loginPageLogo": "",
 		"loginPageSkinLogoUrl": "https://banner2.cleanpng.com/20180513/fpq/kisspng-royalty-free-logo-dragon-5af7c7407136a9.1946411915261878404637.jpg",
 		"loginPageFavicon": "",
@@ -13,6 +13,6 @@ export function carbonioConfig() {
 		"zimbraPublicServicePort": "443",
 		"zimbraPublicServiceProtocol": "https",
 		"zimbraDomainName": "6f4c8e62.testarea.zextras.com",
-		"isDarkThemeEnable": true
+		"isDarkThemeEnable": false
 	}
 }
