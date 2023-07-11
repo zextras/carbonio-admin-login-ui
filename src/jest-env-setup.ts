e1306cc28b5367206773aa5cb060542e902b3899
zextras-vijay <vijay.godhasara@zextras.com>
2023-07-11 11:34:19 +0530

    chore: add jest config and setup file

--- src/jest-env-setup.ts
+++ src/jest-env-setup.ts
@@ -1,11 +1,13 @@
+// Copyright (C) 2011-2020 Zextras
+/* eslint-disable import/no-extraneous-dependencies */
 /*
- * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 import '@testing-library/jest-dom/extend-expect';
-import { server } from './mocks/server';
+import server from './mocks/server';
 
 beforeEach(() => {
 	// Do not useFakeTimers with `whatwg-fetch` if using mocked server
