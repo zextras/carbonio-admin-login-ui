e8eb8c39bfa80b2c5284fcabeda1c05deff31e41
Beatrice Guerra <beatrice.guerra@zextras.com>
2024-07-19 09:27:41 +0200

    build: move to node18, move webpack config to ts, update deps
    Refs: IN-723

--- babel.config.js
+++ babel.config.js
@@ -24,6 +24,7 @@ module.exports = {
 			'i18next-extract',
 			{
 				outputPath: 'translations/{{ns}}.json',
+				discardOldKeys: false,
 				defaultNS: 'en',
 				jsonSpace: 4,
 				compatibilityJSON: 'v4'
