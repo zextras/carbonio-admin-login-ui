9216f378162e6ee995149de95b092df1345473a6
Manan Patel <manan13589@gmail.com>
2026-01-13 17:23:00 +0530

    fix: fix type-check error

--- src/services/advanced-supported.ts
+++ src/services/advanced-supported.ts
@@ -25,7 +25,7 @@ export const getAdvancedSupported = (): GetAdvancedSupportedResponse =>
 		.then(async (res) => {
 			if (res.ok) {
 				const data = await res.json();
-				if ('items' in data && isArray<string>(data.items)) {
+				if ('items' in data && isArray(data.items)) {
 					const installedServices = data.items as Array<string>;
 					const isAdvanced =
 						installedServices.filter((service): boolean => service === 'carbonio-advanced').length >
