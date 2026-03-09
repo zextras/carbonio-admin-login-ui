f4cf0c1509508d1d4564fa531baa0e7671dade7b
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-03-09 17:22:41 +0100

    chore[root](modals): migrate modals.jsx to TypeScript

--- src/components-v1/modals.tsx
+++ src/components-v1/modals.tsx
@@ -4,12 +4,11 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import React from 'react';
-
-import { Modal, Paragraph } from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
 
-export default function OfflineModal({ open, onClose }) {
+import { Modal, Paragraph } from '../ui-components/src/';
+
+export default function OfflineModal({ open, onClose }: any) {
 	const [t] = useTranslation();
 	return (
 		<Modal title="Offline" open={open} onClose={onClose}>
