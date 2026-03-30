928ac8a8db793ff9e1c7c46980801cb0fc7cfd73
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-03-30 18:12:29 +0200

    fix[root] (app): restore conditional rendering for main views

--- src/app.ts
+++ src/app.ts
@@ -52,11 +52,10 @@ export class AppRoot extends LitElement {
     const isSupported = hasSupported && (this.apiResponse as { supported: boolean }).supported;
 
     return html`
-      <ds-snackbar open="" label="qualcosa di lungo che si vede meglio"></ds-snackbar>
-      <!-- ${hasError ? html`<error-page></error-page>` : nothing} -->
-      <!-- ${isLoading ? html`<loading-view></loading-view>` : nothing} -->
-      <!-- ${hasSupported && isSupported ? html`<login-advanced></login-advanced>` : nothing} -->
-      <!-- ${hasSupported && !isSupported ? html`<login-ce></login-ce>` : nothing} -->
+      ${hasError ? html`<error-page></error-page>` : nothing}
+      ${isLoading ? html`<loading-view></loading-view>` : nothing}
+      ${hasSupported && isSupported ? html`<login-advanced></login-advanced>` : nothing}
+      ${hasSupported && !isSupported ? html`<login-ce></login-ce>` : nothing}
     `;
   }
 }
