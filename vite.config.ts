e2061a34ce939e8551ab90cb088f3fa8e8c79c61
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-03-31 12:21:57 +0200

    fix[root](vite.config): rewrite proxy location and cookies
    - Added withLocationRewrite to handle proxy location header rewrites
    - Updated proxy config for /login, /service, and /services
    - Rewrites cookies to remove Secure and SameSite attributes
    - Sets cookie domain to localhost for proxied responses

--- vite.config.ts
+++ vite.config.ts
@@ -72,6 +72,51 @@ function getProxyTarget(): string {
   return `https://${target}:6071`;
 }
 
+function withLocationRewrite(config: {
+  target: string;
+  changeOrigin: boolean;
+  secure: boolean;
+}): object {
+  return {
+    ...config,
+    cookieDomainRewrite: { '*': 'localhost' },
+    configure: (proxy: any) => {
+      proxy.on('proxyReq', (proxyReq: any, req: any) => {
+        const targetUrl = new URL(config.target);
+        proxyReq.setHeader('Origin', targetUrl.origin);
+        if (req.headers['referer']) {
+          proxyReq.setHeader(
+            'Referer',
+            req.headers['referer'].replace('http://localhost:3000', targetUrl.origin),
+          );
+        }
+      });
+
+      proxy.on('proxyRes', (proxyRes: any) => {
+        const cookies = proxyRes.headers['set-cookie'];
+        if (cookies) {
+          if (Array.isArray(cookies)) {
+            proxyRes.headers['set-cookie'] = cookies.map((cookie: string) =>
+              cookie.replace(/;\s*Secure/gi, '').replace(/;\s*SameSite=\w+/gi, ''),
+            );
+          } else if (typeof cookies === 'string') {
+            proxyRes.headers['set-cookie'] = cookies
+              .replace(/;\s*Secure/gi, '')
+              .replace(/;\s*SameSite=\w+/gi, '');
+          }
+        }
+        const location = proxyRes.headers['location'];
+        if (location) {
+          proxyRes.headers['location'] = location.replace(
+            /https:\/\/[^/]+/,
+            'http://localhost:3000',
+          );
+        }
+      });
+    },
+  };
+}
+
 export default defineConfig(({ command, mode }) => {
   const basePath = '/static/login/';
   const isServeCommand = command === 'serve';
@@ -127,28 +172,28 @@ export default defineConfig(({ command, mode }) => {
           server: {
             port: 3000,
             strictPort: false,
-            // proxy: {
-            //   '/zx': {
-            //     target: proxyTarget,
-            //     changeOrigin: true,
-            //     secure: false,
-            //   },
-            //   '/login': withLocationRewrite({
-            //     target: proxyTarget,
-            //     changeOrigin: true,
-            //     secure: false,
-            //   }),
-            //   '/service': withLocationRewrite({
-            //     target: proxyTarget,
-            //     changeOrigin: true,
-            //     secure: false,
-            //   }),
-            //   '/services': withLocationRewrite({
-            //     target: proxyTarget,
-            //     changeOrigin: true,
-            //     secure: false,
-            //   }),
-            // },
+            proxy: {
+              '/zx': {
+                target: proxyTarget,
+                changeOrigin: true,
+                secure: false,
+              },
+              '/login': withLocationRewrite({
+                target: proxyTarget,
+                changeOrigin: true,
+                secure: false,
+              }),
+              '/services': withLocationRewrite({
+                target: proxyTarget,
+                changeOrigin: true,
+                secure: false,
+              }),
+              '/service': withLocationRewrite({
+                target: proxyTarget,
+                changeOrigin: true,
+                secure: false,
+              }),
+            },
           },
         }
       : {}),
